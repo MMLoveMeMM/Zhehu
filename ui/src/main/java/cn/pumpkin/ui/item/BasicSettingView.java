603a3399d725f60a67833d15a1f9cbe9ad3d5b75
liuzhibao <liuzhibao@xl.cn>
2018-12-28 17:20:29 +0800

    增加一个自定义ui件样例

--- ui/src/main/java/cn/pumpkin/ui/item/BasicSettingView.java
+++ ui/src/main/java/cn/pumpkin/ui/item/BasicSettingView.java
@@ -1,9 +1,14 @@
 package cn.pumpkin.ui.item;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import cn.pumpkin.ui.R;
 
 /**
  * @author: zhibao.Liu
@@ -15,15 +20,25 @@ import android.widget.LinearLayout;
 
 public class BasicSettingView extends LinearLayout {
 
+    private TextView mTextView;
+
     public BasicSettingView(Context context) {
         super(context);
     }
 
     public BasicSettingView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        inflate(context, R.layout.basic_setting_view_layout, this);
+        this.setOrientation(HORIZONTAL);
+        this.setGravity(Gravity.CENTER_VERTICAL);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.BasicSettingViewStyle);
+        String name = typedArray.getString(R.styleable.BasicSettingViewStyle_title);
+        String valueHint = typedArray.getString(R.styleable.BasicSettingViewStyle_hint);
+        typedArray.recycle();
+
+        mTextView = (TextView) this.findViewById(R.id.title);
+
     }
 
-    public BasicSettingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
 }
