f6e8803db21326786f123e5f96320de81329a5fb
liuzhibao <liuzhibao@xl.cn>
2019-01-11 19:24:39 +0800

    增加聊天界面,未完

---
+++ app/src/main/java/cn/pumpkin/zhehu/ui/settings/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package cn.pumpkin.zhehu.ui.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.pumpkin.zhehu.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
